8529c019f2f4e981818ea90ce0b6190004b1aff0
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2020-07-11 22:47:12 -0500

    create color theme toggle

--- src/components/color-theme-toggle.tsx
+++ src/components/color-theme-toggle.tsx
@@ -1,22 +1,33 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../utils/color-theme';
+import styles from './color-theme-toggle.module.scss';
 
 const ColorThemeToggle = () => {
   const { colorTheme, setColorTheme } = useContext(ThemeContext);
+
+  const handleToggle = () => {
+    setColorTheme(colorTheme === 'light' ? 'dark' : 'light');
+  };
 
   if (!colorTheme) {
     return null;
   }
 
   return (
-    <label>
+    <div
+      className={styles.toggleContainer}
+      aria-label={`toggle color theme to ${
+        colorTheme === 'light' ? 'dark' : 'light'
+      } mode`}
+    >
       <input
+        id="color-theme-toggle"
         type="checkbox"
         checked={colorTheme === 'dark'}
         onChange={e => setColorTheme(e.target.checked ? 'dark' : 'light')}
-      />{' '}
-      Dark
-    </label>
+      />
+      <div className={styles.toggle} onClick={handleToggle} />
+    </div>
   );
 };
 
--- src/elements/image.tsx
+++ src/elements/image.tsx
@@ -3,15 +3,12 @@ import { getClasses } from '../utils/get-classes';
 import styles from './image.module.scss';
 import { IMAGE_MAX_WIDTH } from '../../gatsby/constants';
 
-const Image: React.FC<ImgHTMLAttributes<HTMLImageElement>> = props => {
-  console.log(props);
-  return (
-    <img
-      width={props.loading === 'lazy' ? IMAGE_MAX_WIDTH : undefined}
-      {...props}
-      className={getClasses(props.className, styles.image)}
-    />
-  );
-};
+const Image: React.FC<ImgHTMLAttributes<HTMLImageElement>> = props => (
+  <img
+    width={props.loading === 'lazy' ? IMAGE_MAX_WIDTH : undefined}
+    {...props}
+    className={getClasses(props.className, styles.image)}
+  />
+);
 
 export default Image;
